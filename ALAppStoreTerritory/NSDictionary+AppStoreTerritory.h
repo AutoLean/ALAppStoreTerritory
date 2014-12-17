b0897298cc9ff9980c921505574a235db8647b5f
Jeffrey Jackson <jj@autolean.com>
2014-12-17 18:23:18 -0500

    Create group for Classes

---
+++ ALAppStoreTerritory/NSDictionary+AppStoreTerritory.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+AppStoreTerritory.h
+//  ALAppStoreTerritory
+//
+//  Created by Jeffrey Jackson on 10/13/14.
+//  Copyright (c) 2014 AutoLean, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (AppStoreTerritory)
+
++(instancetype)territories;
+
+@end
